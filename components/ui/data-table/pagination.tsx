7fa54c4c06942918da3c5b4a9bd18d4877f0f230
Yisehak Abreham <yisehak.aw@gmail.com>
2025-04-20 15:56:18 +0300

    simplify pagination component

--- components/ui/data-table/pagination.tsx
+++ components/ui/data-table/pagination.tsx
@@ -23,12 +23,11 @@ export function DataTablePagination<TData>({
 }: DataTablePaginationProps<TData>) {
   return (
     <div className="flex items-center justify-between px-2">
-      <div className="flex-1 text-sm text-muted-foreground">
-        {table.getFilteredSelectedRowModel().rows.length} of{" "}
-        {table.getFilteredRowModel().rows.length} row(s) selected.
-      </div>
+      {/* <div className="flex-1 text-sm text-muted-foreground">
+        {table.getFilteredSelectedRowModel().rows.length} selected
+      </div> */}
       <div className="flex items-center space-x-6 lg:space-x-8">
-        <div className="flex items-center space-x-2">
+        {/* <div className="flex items-center space-x-2">
           <p className="text-sm font-medium">Rows per page</p>
           <Select
             value={`${table.getState().pagination.pageSize}`}
@@ -47,13 +46,13 @@ export function DataTablePagination<TData>({
               ))}
             </SelectContent>
           </Select>
-        </div>
+        </div> */}
         <div className="flex w-[100px] items-center justify-center text-sm font-medium">
           Page {table.getState().pagination.pageIndex + 1} of{" "}
           {table.getPageCount()}
         </div>
         <div className="flex items-center space-x-2">
-          <Button
+          {/* <Button
             variant="outline"
             className="hidden h-8 w-8 p-0 lg:flex"
             onClick={() => table.setPageIndex(0)}
@@ -61,7 +60,7 @@ export function DataTablePagination<TData>({
           >
             <span className="sr-only">Go to first page</span>
             <ChevronsLeft />
-          </Button>
+          </Button> */}
           <Button
             variant="outline"
             className="h-8 w-8 p-0"
@@ -80,7 +79,7 @@ export function DataTablePagination<TData>({
             <span className="sr-only">Go to next page</span>
             <ChevronRight />
           </Button>
-          <Button
+          {/* <Button
             variant="outline"
             className="hidden h-8 w-8 p-0 lg:flex"
             onClick={() => table.setPageIndex(table.getPageCount() - 1)}
@@ -88,7 +87,7 @@ export function DataTablePagination<TData>({
           >
             <span className="sr-only">Go to last page</span>
             <ChevronsRight />
-          </Button>
+          </Button> */}
         </div>
       </div>
     </div>
