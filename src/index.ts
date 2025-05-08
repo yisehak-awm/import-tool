2fcd9e40c343e75d48efd2ca3fcfd81e90b960ce
Yisehak Abreham <yisehak.aw@gmail.com>
2025-05-08 11:37:36 +0300

    add export for more types

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import SchemaBuilder from "./builder";
 import CSVuploader from "./csv-uploader";
 import DatasourceList from "./datasource-list";
 import Context, { DataSource, Schema } from "./context";
+import { Entity, EntityData } from "./entity";
+import { Relation, RelationData } from "./relation";
 
 export {
   SchemaBuilder,
@@ -10,4 +12,8 @@ export {
   Context,
   DataSource,
   Schema,
+  Entity,
+  EntityData,
+  Relation,
+  RelationData,
 };
